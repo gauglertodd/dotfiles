afc5a77b8325ba7123808d9de6a9168af12bfd00
Todd <toddg@visibleworld.com>
2017-11-17 12:12:55 -0500

    adding installation script

---
+++ .emacs.d/install.el
@@ -0,0 +1,45 @@
+;; run with emasc --script install.el
+(require 'package)
+
+; find package information from following archives
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+(package-initialize)
+
+(mapcar (lambda (package)
+          ; install package if not already installed
+          (unless (package-installed-p package)
+            (package-install package)))
+        ; list of packages to be installed
+        '(yasnippet
+          async
+          auctex
+          auto-complete
+          concurrent
+          ctable
+          dash
+          defferred
+          epc
+          epl
+          format-sql
+          git-commit
+          jedi
+          jedi-core
+          js-auto-beautify
+          js2-mode
+          latex-preview-pane
+          magit
+          magit-popup
+          pkg-info
+          popup
+          projectil
+          py-yapf
+          python-environment
+          web-beautify
+          web-mode
+          with-editor
+          yapfify
+          yasnipped
+          color-theme))
