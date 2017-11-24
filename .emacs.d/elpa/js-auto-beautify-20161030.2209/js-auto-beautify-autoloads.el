6004b051c34b2471466d9f4f70e9da1a0c8e5ed1
Todd Gaugler <gauglertodd@gmail.com>
2017-11-24 11:23:55 -0500

    Removing stuff

--- .emacs.d/elpa/js-auto-beautify-20161030.2209/js-auto-beautify-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; js-auto-beautify-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "js-auto-beautify" "js-auto-beautify.el" (22958
-;;;;;;  61499 504679 67000))
-;;; Generated autoloads from js-auto-beautify.el
-
-(autoload 'js-auto-beautify-mode "js-auto-beautify" "\
-auto-beautify you js/jsx
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; js-auto-beautify-autoloads.el ends here
