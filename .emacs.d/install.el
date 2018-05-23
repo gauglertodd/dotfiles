59afde0cb270293602c715bbb756ec1e08f1b158
Todd <todd.gaugler@frockhub.com>
2018-05-23 17:42:18 -0400

    Update install.el

--- .emacs.d/install.el
+++ .emacs.d/install.el
@@ -5,7 +5,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
-
+(package-refresh-content)
 (package-initialize)
 
 (mapcar (lambda (package)
@@ -42,6 +42,4 @@
           yapfify
           color-theme
           ess
-          sphinx-doc
-          exec-path-from-shell-initialize,
-          clang-format))
+          sphinx-doc))
