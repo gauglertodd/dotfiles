989fa4a4b03a43d4c4133b23927c2f521f012975
Todd <todd.gaugler@frockhub.com>
2018-05-23 17:41:38 -0400

    Update install.el

--- .emacs.d/install.el
+++ .emacs.d/install.el
@@ -5,7 +5,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
-
+(package-refresh-content)
 (package-initialize)
 
 (mapcar (lambda (package)
